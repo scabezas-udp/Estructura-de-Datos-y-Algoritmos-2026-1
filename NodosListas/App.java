79e2d4f1e848cbb433fe2ca35782e45d53d33162
Sebastián Cabezas | UDP <sebastian.cabezas.r@gmail.com>
2026-03-26 12:32:02 -0300

    Clase Ejecutable

---
+++ NodosListas/App.java
@@ -0,0 +1,8 @@
+package NodosListas;
+
+public class App {
+    public static void main(String[] args) {
+        ListaSimplementeEnlazada lse = new ListaSimplementeEnlazada();
+        lse.mostrar();
+    }
+}
