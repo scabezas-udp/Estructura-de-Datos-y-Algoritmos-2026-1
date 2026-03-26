f316268ad3bd59bf480fea10a4c847ff10839872
Sebastián Cabezas | UDP <sebastian.cabezas.r@gmail.com>
2026-03-26 12:20:40 -0300

    Clase Nodo

---
+++ NodosListas/NodoEnteros.java
@@ -0,0 +1,13 @@
+package NodosListas;
+
+public class NodoEnteros {
+
+    int dato;
+    NodoEnteros siguiente;
+
+    public NodoEnteros(int _dato){
+        this.dato = _dato;
+        this.siguiente = null;
+    }
+    
+}
