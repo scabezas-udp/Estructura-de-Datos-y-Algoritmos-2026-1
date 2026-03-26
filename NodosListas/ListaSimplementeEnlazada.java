818f06f3138722ee1ae4d387f97fe071cdcfe306
Sebastián Cabezas | UDP <sebastian.cabezas.r@gmail.com>
2026-03-26 12:28:37 -0300

    Clase ListaSE con funcion mostrar

--- NodosListas/ListaSimplementeEnlazada.java
+++ NodosListas/ListaSimplementeEnlazada.java
@@ -3,13 +3,29 @@ package NodosListas;
 public class ListaSimplementeEnlazada {
     // primero de la lista
     NodoEnteros head;
+
     // constructor por defecto (), construye una lista vacia
-    public ListaSimplementeEnlazada(){
+    public ListaSimplementeEnlazada() {
         this.head = null;
     }
-    
-    public boolean estaVacia(){
+
+    public boolean estaVacia() {
         return this.head == null;
+    }
+
+    public void mostrar() {
+        if (estaVacia()) {
+            System.out.println("--- Está Vacía ---");
+        } else {
+            // recorrido -> aux
+            NodoEnteros aux = this.head;
+            while (aux != null) {
+                System.out.println("[" + aux.dato + "] --> " + aux.siguiente);
+                //sout("[" + aux.dato + "] --> " + aux.siguiente);
+                aux = aux.siguiente;
+            }
+        }
+        System.out.println("------------------");
     }
 
 }
