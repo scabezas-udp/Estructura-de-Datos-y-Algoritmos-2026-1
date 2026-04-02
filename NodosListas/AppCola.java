8ab560d3e3b8afdfd6f5fa4f142f6bdc4a0e1687
Sebastián Cabezas Ríos <68340396+scabezas-academico@users.noreply.github.com>
2026-04-02 18:30:26 +0000

    Colas

---
+++ NodosListas/AppCola.java
@@ -0,0 +1,33 @@
+package NodosListas;
+
+public class AppCola {
+    public static void main(String[] args) {
+        Cola c1 = new Cola();
+        int nuevoDato;
+        System.out.println("Cola vacia?: " + c1.isEmpty());
+        nuevoDato = 9;
+        System.out.println("Se inserto el ["+nuevoDato+"]?: " + c1.add(nuevoDato));
+        System.out.println("Cola vacia?: " + c1.isEmpty());
+        nuevoDato = 5;
+        System.out.println("Se inserto el ["+nuevoDato+"]?: " + c1.add(nuevoDato));
+        nuevoDato = 1;
+        System.out.println("Se inserto el ["+nuevoDato+"]?: " + c1.add(nuevoDato));
+        
+        // quitar
+        NodoEnteros elementoQuitado;
+        try {
+            elementoQuitado = c1.remove();
+            System.out.println("Se quito el ["+elementoQuitado.dato+"]");
+            elementoQuitado = c1.remove();
+            System.out.println("Se quito el ["+elementoQuitado.dato+"]");
+            elementoQuitado = c1.remove();
+            System.out.println("Se quito el ["+elementoQuitado.dato+"]");
+            elementoQuitado = c1.remove();
+            System.out.println("Se quito el ["+elementoQuitado.dato+"]");
+        } catch (Exception e) {
+            System.out.println("Error al quitar: " + e.toString());
+        }
+
+        System.out.println("--- Fin Colas ---");
+    }
+}
---
+++ NodosListas/Cola.java
@@ -0,0 +1,48 @@
+package NodosListas;
+
+public class Cola {
+    
+    private NodoEnteros frente; // Es el primer elemento
+    
+    public Cola() {
+        this.frente = null;
+    }
+
+    // Cola Vacia
+    public boolean isEmpty(){
+        return this.frente == null;
+    }
+
+    // Insertar al Final -> Enqueue -> Add
+    public boolean add(int _dato){
+        try {
+            NodoEnteros nuevo = new NodoEnteros(_dato);
+            if (isEmpty()) {
+                this.frente = nuevo;
+                return true;
+            } else {
+                NodoEnteros aux = this.frente;
+                while(aux.siguiente != null){
+                    aux = aux.siguiente;
+                }
+                aux.siguiente = nuevo;
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Quitar el Primero -> Remove -> Dequeue
+    public NodoEnteros remove() throws Exception{
+        
+        if (isEmpty()) {
+            throw new Exception("Cola Vacia");
+        }
+        NodoEnteros aux = this.frente;
+        this.frente = this.frente.siguiente;
+
+        return aux;   
+    }
+
+}
